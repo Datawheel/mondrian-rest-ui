1a1b33c229613e48dc937329cc0ad70e4149caaa
Manuel Aristarán <jazzido@jazzido.com>
2017-05-25 19:58:46 -0400

    better drilldown and cut menus

--- src/components/DrillDownMenu.js
+++ src/components/DrillDownMenu.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { isNull } from 'lodash';
 import PropTypes from 'prop-types';
 
-import { NavDropdown } from 'react-bootstrap';
+import { DropdownButton } from 'react-bootstrap';
 
 import { addDrilldown } from '../redux/reducers/aggregation';
 import '../css/DrillDownMenu.css';
@@ -12,25 +11,25 @@ export default function DrillDownMenu(props, context) {
           { dispatch } = context.store,
     dimensions = cube ? cube.dimensions : [];
 
-    return (
-        <NavDropdown title="Add Drilldown" disabled={isNull(cube)}>
-            {dimensions.map((d,i) =>
-            <li key={i} className="dropdown-submenu">
-                <a tabIndex="-1" href="#">{d.name}</a>
-                <ul className="dropdown-menu">
-                    {d.hierarchies[0]
-                    .levels
-                    .slice(1)
-                      .map((l,j) =>
-                          <li key={`${i}.${j}`}
-                              onClick={() => dispatch(addDrilldown(l))}>
-                              <a tabIndex="-1" href="#">{ drillDowns.find(dd => dd === l) ? '✓ ' : ' ' }{l.name}</a>
-                          </li>
-                    )}
-                </ul>
-            </li>)
+  return (
+    <DropdownButton bsSize="xsmall" title="Add Drilldown" id="dropdown-size-small" disabled={dimensions.length === 0} style={{marginRight: '10px' }}>
+      {dimensions.map((d,i) =>
+      <li key={i} className="dropdown-submenu">
+        <a tabIndex="-1" href="#">{d.name}</a>
+        <ul className="dropdown-menu">
+          {d.hierarchies[0]
+          .levels
+          .slice(1)
+          .map((l,j) =>
+          <li key={`${i}.${j}`}
+              onClick={() => dispatch(addDrilldown(l))}>
+            <a tabIndex="-1" href="#">{ drillDowns.find(dd => dd === l) ? '✓ ' : ' ' }{l.name}</a>
+          </li>
+          )}
+        </ul>
+      </li>)
             }
-        </NavDropdown>
+      </DropdownButton>
     );
 }
 
