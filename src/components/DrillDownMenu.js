9e731e9da8b3a21f7b33c655dd8f8b5be5bed05c
Manuel Aristarán <jazzido@jazzido.com>
2017-06-27 18:13:16 -0400

    starting with multiple hierarchies

--- src/components/DrillDownMenu.js
+++ src/components/DrillDownMenu.js
@@ -1,38 +1,60 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
 
-import { DropdownButton } from 'react-bootstrap';
+import { DropdownButton } from "react-bootstrap";
 
-import { addDrilldown } from '../redux/reducers/aggregation';
-import '../css/DrillDownMenu.css';
+import { addDrilldown } from "../redux/reducers/aggregation";
+import "../css/DrillDownMenu.css";
+
+function levels(hierarchy, drillDowns, dispatch) {
+  return (
+    <ul className="dropdown-menu">
+      {hierarchy.levels
+        .slice(hierarchy.allMemberName === null ? 0 : 1)
+        .map((l, li) =>
+          <li key={li} onClick={() => dispatch(addDrilldown(l))}>
+            <a>
+              {drillDowns.find(dd => dd === l) ? "✓ " : " "}
+              {l.name}
+            </a>
+          </li>
+        )}
+    </ul>
+  );
+}
 
 export default function DrillDownMenu(props, context) {
-    const { cube, drillDowns } = props,
-          { dispatch } = context.store,
+  const { cube, drillDowns, dispatch } = props,
     dimensions = cube ? cube.dimensions : [];
 
   return (
-    <DropdownButton bsSize="xsmall" title="Add Drilldown" id="dropdown-size-small" disabled={dimensions.length === 0} style={{marginRight: '10px' }}>
-      {dimensions.map((d,i) =>
-      <li key={i} className="dropdown-submenu">
-        <a tabIndex="-1">{d.name}</a>
-        <ul className="dropdown-menu">
-          {d.hierarchies[0]
-          .levels
-          .slice(1)
-          .map((l,j) =>
-          <li key={`${i}.${j}`}
-              onClick={() => dispatch(addDrilldown(l))}>
-            <a tabIndex="-1">{ drillDowns.find(dd => dd === l) ? '✓ ' : ' ' }{l.name}</a>
+    <DropdownButton
+      bsSize="xsmall"
+      title="Add Drilldown"
+      id="dropdown-size-small"
+      disabled={dimensions.length === 0}
+      style={{ marginRight: "10px" }}
+    >
+      {dimensions.map((d, i) => {
+        return (
+          <li key={i} className="dropdown-submenu">
+            <a tabIndex="-1">
+              {d.name}
+            </a>
+            {d.hierarchies.length > 1
+              ? <ul className="dropdown-menu">
+                  {d.hierarchies.map((h, hi) =>
+                    <li key={hi} className="dropdown-submenu">
+                      <a>
+                        {h.name}
+                      </a>
+                      {levels(h, drillDowns, dispatch)}
+                    </li>
+                  )}
+                </ul>
+              : levels(d.hierarchies[0], drillDowns, dispatch)}
           </li>
-          )}
-        </ul>
-      </li>)
-            }
-      </DropdownButton>
-    );
+        );
+      })}
+    </DropdownButton>
+  );
 }
-
-DrillDownMenu.contextTypes = {
-    store: PropTypes.object.isRequired
-};
