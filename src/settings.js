5f64aaa42a9a8bf974ad056d83d731fcd00d5594
Manuel Aristarán <jazzido@jazzido.com>
2017-11-01 21:21:46 -0300

    allow overriding settings from `window`

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,8 @@
 import { Client } from 'mondrian-rest-client';
 
-export const __API_ENDPOINT__ = process.env.REACT_APP_API_URL || 'http://localhost:9292/';
+export const __API_ENDPOINT__ = window.__API_ENDPOINT__ || process.env.REACT_APP_API_URL || 'http://localhost:9292/';
+
 export const client = new Client(__API_ENDPOINT__);
 export const PAGE_SIZE = 40;
 
-export const SITE_TITLE = process.env.SITE_TITLE || 'mondrian-rest-ui';
+export const SITE_TITLE = window.__SITE_TITLE__ || process.env.SITE_TITLE || 'mondrian-rest-ui';
