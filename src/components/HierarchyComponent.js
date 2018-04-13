6ef3fb2761ac46cbe1b3b7ee276ffdf3b6a12384
Jonathan Speiser <jspeis@gmail.com>
2018-04-13 12:55:20 -0400

    implements @hwchen suggestion to only show deeper menus if needed

--- src/components/HierarchyComponent.js
+++ src/components/HierarchyComponent.js
@@ -4,6 +4,13 @@ export const HierarchyComponent = props => {
   const { hierarchy, clickEvent } = props;
   const name = hierarchy.name;
   const levels = hierarchy.levels.slice(hierarchy.allMemberName ? 1 : 0);
+  if (levels.length === 1) {
+    return (
+      <li onClick={() => clickEvent(levels[0])} key={0}>
+        <a tabIndex="-1">{levels[0].name}</a>
+      </li>
+    );
+  }
   return (
     <li className="dropdown-submenu">
       <a tabIndex="-1">{name}</a>
